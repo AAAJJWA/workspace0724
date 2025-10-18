a44aa34531d0401b2cbb1d6f5812491be2cdc98e
AAAJJWA <lsh00071450@gmail.com>
2025-10-18 21:22:30 +0900

    no message

---
+++ 04_Servlet/mybatisProject/src/main/java/com/kh/mybatis/controller/member/AjaxIdCheckController.java
@@ -0,0 +1,50 @@
+package com.kh.mybatis.controller.member;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import com.kh.mybatis.service.MemberService;
+
+/**
+ * Servlet implementation class AjaxIdCheckController
+ */
+@WebServlet("/idDuplicateCheck.me")
+public class AjaxIdCheckController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AjaxIdCheckController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String checkId = request.getParameter("checkId");
+		
+		int count = new MemberService().idCheck(checkId);
+		
+		if(count > 0) {	// 회원이 존재
+			response.getWriter().print("NNNN");
+		} else {	// 회원이 존재하지 않음
+			response.getWriter().print("NNNY");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
