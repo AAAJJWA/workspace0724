396024d6a9d932118c3a6b8fd14ba83d0360038b
AAAJJWA <lsh00071450@gmail.com>
2025-10-22 14:14:55 +0900

    no message

---
+++ 05_Spring/spring/src/main/java/service/MemberService.java
@@ -0,0 +1,11 @@
+package service;
+
+import com.kh.spring.model.vo.Member;
+
+public interface MemberService {
+
+    @Override
+    Member loginMember(Member member) {
+        return null;
+    }
+}
