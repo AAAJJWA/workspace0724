b1cf5e87bd8a7e4e5a652ef08941ed88a6504223
AAAJJWA <lsh00071450@gmail.com>
2025-11-26 15:15:59 +0900

    no message

--- 06_REACT/react-project/src/App.jsx
+++ 06_REACT/react-project/src/App.jsx
@@ -1,11 +1,26 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
+import UseStateTest from './components/useState/UseStateTest'
+import Signup from './components/useState/Signup'
+import LandingPage from './components/useState/LandingPage'
+import UseRefTest from './components/useRef/useRefTest'
+import UseRefScroll from './components/useRef/useRefScroll'
+import UseMemoTest from './components/useMemo/useMemoTest'
+import UseCallbackTest from './components/useCallback/useCallbackTest'
 
 function App() {
 
   return (
     <>
-
+      {/* <UseStateTest /> */}
+      {/* <Signup /> */}
+      {/* <LandingPage /> */}
+      {/* <UseRefTest /> */}
+      {/* <UseRefScroll /> */}
+      {/* <UseMemoTest /> */}
+      <UseCallbackTest />
     </>
   )
 }
