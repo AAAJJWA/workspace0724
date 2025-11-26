7c030c85e9834ea0ff234a66ce034aa3ef97411a
AAAJJWA <lsh00071450@gmail.com>
2025-11-26 10:04:41 +0900

    no message

--- 06_REACT/react-project/src/App.jsx
+++ 06_REACT/react-project/src/App.jsx
@@ -1,15 +1,11 @@
 import { useState } from 'react'
-import JavaScript from './components/JavaScript'
 import './App.css'
-import Style from './components/Style'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      {/* <JavaScript /> */}
-      <Style />
+
     </>
   )
 }
