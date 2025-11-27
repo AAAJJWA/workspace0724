c473206ceace0c825441715383918b1568b8fa42
AAAJJWA <lsh00071450@gmail.com>
2025-11-27 14:14:21 +0900

    no message

--- 06_REACT/react-project/src/App.jsx
+++ 06_REACT/react-project/src/App.jsx
@@ -2,13 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UseStateTest from './components/useState/UseStateTest'
 import Signup from './components/useState/Signup'
 import LandingPage from './components/useState/LandingPage'
-import UseRefTest from './components/useRef/useRefTest'
-import UseRefScroll from './components/useRef/useRefScroll'
-import UseMemoTest from './components/useMemo/useMemoTest'
-import UseCallbackTest from './components/useCallback/useCallbackTest'
-import UseEffectTest from './components/useEffect/UseEffectTest'
+import UseRefTest from './components/useRef/UseRefTest'
+import UseRefScroll from './components/useRef/UseRefScroll'
+import UseMemoTest from './components/useMemo/UseMemoTest'
+import UseCallbackTest from './components/useCallback/UseCallbackTest'
+import UseEffectView from './components/useEffect/UseEffectView'
+import UseContextTest from './components/useContext/UseContextTest'
+import MyInfo from './components/customHook/MyInfo'
+import ToggleBox from './components/customHook/ToggleBox'
+import { UserProvider } from './components/useContext/UserContext'
+import Header from './components/useContext/Header'
 
 function App() {
 
@@ -21,8 +27,15 @@ function App() {
       {/* <UseRefScroll /> */}
       {/* <UseMemoTest /> */}
       {/* <UseCallbackTest /> */}
-      <UseEffectTest />
-    </>
+      {/* <UseEffectView /> */}
+      {/* <UseContextTest /> */}
+      {/* <MyInfo /> */}
+      {/* <ToggleBox /> */}
+      <UserProvider>
+        <Header />
+      </UserProvider>
+
+    </> 
   )
 }
 
--- 06_REACT/react-project/src/components/useEffect/UseEffectView.jsx
+++ 06_REACT/react-project/src/components/useEffect/UseEffectView.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import UseEffectTest from './UseEffectTest';
+import React, { useState } from 'react'
+import UesEffectTest from './UesEffectTest';
 
 const UseEffectView = () => {
     const [isView, setIsView] = useState(false);
-
+  
     return (
         <div>
-            <button onClick = {() => setIsView(prev => !prev)}>
+            <button onClick={() => setIsView(prev => !prev)}>
                 화면출력
             </button>
             {
-                isView && <UseEffectTest />
+                isView &&
+                <UesEffectTest />
             }
-
         </div>
     )
 }
